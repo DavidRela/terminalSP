27c56436d2629f6fcf469817466004aedc10e430
David Rela <daverela@gmail.com>
2016-09-05 12:27:58 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,10 +1,10 @@
 //Function to load a random image each page load.
 
 //Creating variables
-var imgs = ["url(../img/img0.jpg)", 
-	"url(../img/img1.jpg)",
-	"url(../img/img2.jpg)",
-	"url(../img/img3.jpg)"
+var imgs = ["url(img/img0.jpg)", 
+	"url(img/img1.jpg)",
+	"url(img/img2.jpg)",
+	"url(img/img3.jpg)"
 	];
 	
 //Get a random value from the "imgs" array

740398d8b7d00df9856654d44b7f206fa85788c5
David Rela <daverela@gmail.com>
2016-09-05 12:20:47 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,10 +1,10 @@
 //Function to load a random image each page load.
 
 //Creating variables
-var imgs = ["../img/img0.jpg", 
-	"../img/img1.jpg",
-	"../img/img2.jpg",
-	"../img/img3.jpg"
+var imgs = ["url(../img/img0.jpg)", 
+	"url(../img/img1.jpg)",
+	"url(../img/img2.jpg)",
+	"url(../img/img3.jpg)"
 	];
 	
 //Get a random value from the "imgs" array
@@ -12,5 +12,5 @@ var rnd = imgs[Math.floor(Math.random() * imgs.length)];
 
 //Function
 $( document ).ready(function() {
-    $( "#rndimg" ).css( "background-image" , "../img/img0.jpg" );
+    $( "#rndimg" ).css( "background-image" , rnd );
 });

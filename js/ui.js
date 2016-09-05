c8ed200c5fac0a350ea59041fe7ae0014852775c
David Rela <daverela@gmail.com>
2016-09-05 12:15:38 +0200

    Updated to pick random images

--- js/ui.js
+++ js/ui.js
@@ -1,10 +1,16 @@
-// postition won't work correctly if you
-// use a custom userChrome.css that modifies
-// the browsers default content display.
-// UI mods should not affect it.
-$(document).ready(function() {
-	$("#container").draggable().position({});
-	$("#container").mousedown(function() {
-        	$("#container").removeAttr('style');
-    })
+//Function to load a random image each page load.
+
+//Creating variables
+var imgs = ["../img/img0.jpg", 
+	"../img/img1.jpg",
+	"../img/img2.jpg",
+	"../img/img3.jpg"
+	];
+	
+//Get a random value from the "imgs" array
+var rnd = imgs[Math.floor(Math.random() * imgs.length)];
+
+//Function
+$( document ).ready(function() {
+    $( "#rndimg" ).css( "background-image" , rnd );
 });

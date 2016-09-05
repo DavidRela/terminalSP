cdb2b3cb88e621a9d87bc93653afdff883716fe7
David Rela <daverela@gmail.com>
2016-09-05 12:21:24 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -12,5 +12,5 @@ var rnd = imgs[Math.floor(Math.random() * imgs.length)];
 
 //Function
 $( document ).ready(function() {
-    $( "#rndimg" ).css( "background-image" , rnd );
+    $( "#rndimg" ).css( "background" , rnd );
 });
